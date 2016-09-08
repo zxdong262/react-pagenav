d415046fc1a31cb781163c8741362aa1221888ac
ZHAO Xudong <zxdong@gmail.com>
2016-09-08 08:40:06 +0800

    dev new edition

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
-var webpack = require('webpack')
-var path = require('path')
+const webpack = require('webpack')
 
-module.exports = {
-	entry: {
-		app: './src/index.jsx',
-	},
-	output: {
-		filename: 'bundle.js',
-		publicPath: '/lib/',
-		path: path.resolve(__dirname, 'lib')
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.optimize.OccurrenceOrderPlugin()
-	],
-	devtool: '#eval-source-map',
-	module: {
-		loaders: [
-			{
-				test: /\.js[x]?$/,
-				loaders: ['babel'],
-				include: path.join(__dirname, '/src')
-			}
-			,{
-				test: /\.(html|css)$/,
-				loader: 'raw-loader'
-			}
-		]
-	}
+var config = {
+  entry: {
+    'react-pagenav': './src/react-pagenav.jsx',
+    app: './src/app.js'
+  },
+  output: {
+    path: __dirname + '/dist/',//输出文件目录
+    filename: '[name].js', //输出文件名
+    libraryTarget: 'umd'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: '#eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
+
+if(process.env.NODE_ENV === 'production') {
+
+  config.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': "'production'"
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle:   true,
+      compress: {
+          warnings: false, // Suppress uglification warnings
+      }
+    })
+  ]
+
+  config.devtool = 'source-map'
+
+}
+
+
+module.exports = config
